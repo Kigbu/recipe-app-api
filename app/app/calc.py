5564e8d57a9f0bb49c4b34d14a8b1ee11734d981
kigbua <kigbua@gmail.com>
2023-12-05 08:12:28 +0100

    install postgresql

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """ Add x anf y and return result."""
+    return x + y
+
+
+def substract(x, y):
+    """ Substract x from y and return result"""
+    return y - x
