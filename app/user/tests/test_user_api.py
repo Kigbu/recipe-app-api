308ce9c29b4c8b1a47175c1f8949f95f10c7c939
kigbua <kigbua@gmail.com>
2023-12-13 19:04:33 +0100

    user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -108,6 +108,14 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
 
 class PrivateUserApiTests(TestCase):
     """Test API requests that requires authentication."""
